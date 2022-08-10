a2941f0dd8644e5c4c549c262d03ebc432cb97eb
Michael Gerfen <gerfen@hotmail.com>
2022-08-09 17:28:15 -0700

    WIP:  Adding ProjectDesignSurface control.

---
+++ ProjectDesignSurface/Utils/AbstractModelBase.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace ClearDashboard.ProjectDesignSurface.Utils
+{
+    /// <summary>
+    /// Abstract base for view-model classes that need to implement INotifyPropertyChanged.
+    /// </summary>
+    public abstract class AbstractModelBase : INotifyPropertyChanged
+    {
+#if DEBUG
+        private static int _nextObjectId = 0;
+        private int _objectDebugId = _nextObjectId++;
+
+        public int ObjectDebugId => _objectDebugId;
+#endif //  DEBUG
+
+        /// <summary>
+        /// Raises the PropertyChanged event.
+        /// </summary>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Event raised to indicate that a property value has changed.
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
